5eeabdaa2266bd4e5f5e7202e887fbc280d7e1c2
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-12-30 10:31:05 +0700

    produce solution for 1. two-sum

--- problems/code.cpp
+++ problems/code.cpp
@@ -6,44 +6,46 @@
 #include <sstream>
 
 
+template <typename T>
+void print_vector(const std::vector<T> vect) {
+    for (const auto& index : vect) {
+        std::cout << index << " ";
+    };
+    std::cout << std::endl;
+}
+
+
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
         std::vector<int> result;
-
-        int length { static_cast<int>(nums.size()) };
-            
         std::map<int, int> dictionary;
-        
+        int length { static_cast<int>(nums.size()) };            
+
         for (std::size_t index{ 0 }; index < length; ++index) {
-            if (nums[index] < target) {
-                for(auto const& [key, val] : dictionary) {
-                    if(val + nums[index] == target) {
-                        result.insert(result.end(), {val, static_cast<int>(index)});
-                        break;
-                    }
-                }
-
-                dictionary[index] = nums[index];
+            for(auto const& [key, val] : dictionary) {
+                if(val + nums[index] == target) {
+                    result.insert(result.end(), {key, static_cast<int>(index)});
+                    break;
+                };
             };
 
-            if (static_cast<int>(nums.size()) > 0) break;
-            };
+        dictionary[index] = nums[index];
+
+        if (static_cast<int>(result.size()) > 0) break;
+        };
 
         return result;
-        }
-
-        // smth
-
-
+        };
 };
+
 
 int main() {
     int number;
     std::vector<int> nums;
     std::string input_line;
 
-    std::cout << "Array of nums:" << "\n";
+    std::cout << "Array of nums: ";
 
     std::getline(std::cin, input_line);
     std::istringstream stream(input_line);
@@ -65,18 +67,15 @@ int main() {
 
 
     int target;
-    std::cout << "target value:" << "\n";
+    std::cout << "target value: ";
     std::cin >> target;
 
 
     Solution solution;
     std::vector<int> result { solution.twoSum(nums, target) };
 
-    std::cout << "Result: \n";
-    for (const auto& index : result) {
-        std::cout << index << " ";
-        }
-    std::cout << std::endl;
+    std::cout << "Result: ";
+    print_vector(result);
 
 
     return 0;
