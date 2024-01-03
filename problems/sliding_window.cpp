682324b35ada9ccf0f2275c7eb4fc45ee2415e00
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-03 13:43:15 +0700

    sliding window algorithm

---
+++ problems/sliding_window.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <numeric>
+#include "utils.h"
+
+// Helps to solve O(nk) by O(n) + O(k)
+
+using namespace utils;
+
+template <typename T>
+std::vector<T> window_sum(const std::vector<T> vect, const int w_size)
+{
+    std::vector<T> result;
+    
+    T w_sum = 0;
+    for (std::size_t i = 0; i < w_size; ++i)
+        w_sum += vect[i];
+
+    result.push_back(w_sum);
+
+
+    for (std::size_t i = w_size; i < vect.size(); ++i)
+        result.push_back(result.back() + vect[i] - vect[i - w_size]);
+
+    return result;
+}
+
+
+int main()
+{
+    std::vector<int> v(100);
+    std::iota (std::begin(v), std::end(v), 0);
+
+    print_vector(window_sum(v, 3));
+
+    return 0;
+};
