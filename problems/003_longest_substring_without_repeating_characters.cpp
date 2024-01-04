6133c586a70042dfd1c8af96e546fe1cd7acf4ba
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-04 22:59:34 +0700

    find optimal solution for 003

--- problems/003_longest_substring_without_repeating_characters.cpp
+++ problems/003_longest_substring_without_repeating_characters.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <algorithm>
 
 
 class Solution {
 public:
     int lengthOfLongestSubstring(std::string s) {
-        int length;
-        std::string subs;
+        int length = 0;
+        std::string subs = "";
+        std::unordered_map<char, int> m;
 
+        int i = 0;
         for (char& letter: s)
         {
-            std::cout << subs;
-            // subs.erase(subs.begin());
+            if (m.find(letter) != m.end())
+                subs.erase(0, subs.find(letter) + 1);
+
             subs += letter;
+            m[letter] = i;
+
+            std::cout << subs << "\n";
+
+            length = std::max(length, static_cast<int>(subs.size()));
+
+            i++;
         };
+
         return length;
     }
 };
@@ -21,18 +34,11 @@ public:
 
 int main()
 {
-    std::string name;
+    std::string s;
+    std::cin >> s;                                          // OR std::getline(std::cin, s);
 
-    std::cout << "Please, enter your full name: ";
-    std::getline (std::cin,name);
-    std::cout << "Hello, " << name << "!\n";
-
-    // std::string s;
-    // // std::cin >> s;                                           // Segmentation fault (core dumped) ????????
-    // std::getline(std::cin, s);
- 
-    // Solution solution;
-    // std::cout << solution.lengthOfLongestSubstring(s) + "\n";
+    Solution solution;
+    std::cout << solution.lengthOfLongestSubstring(s) << "\n";
 
     return 0;
 };
