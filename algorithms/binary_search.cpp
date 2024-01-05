e2fe88a26d09ac285becced39e0303288fae76ca
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-05 15:39:54 +0700

    add recursive solution

--- algorithms/binary_search.cpp
+++ algorithms/binary_search.cpp
@@ -7,7 +7,7 @@ using namespace utils;
 
 
 template <typename T>
-T iterative_binary_search(std::vector<T> vect, T x)
+T iterative_binary_search(const std::vector<T>& vect, T x)
 {
     int l = 0;
     int m;
@@ -26,19 +26,46 @@ T iterative_binary_search(std::vector<T> vect, T x)
      };
     };
 
-    return NULL;
-}
+    return -1;
+};
+
+
+
+template <typename T>
+T recursive_binary_search(const std::vector<T>& vect, T x, int l, int r)
+{
+    int m = l + (r - l) / 2;
+
+    if (vect[m] == x) {
+        return m;
+    } else if (vect[m] < x) {
+        return recursive_binary_search(vect, x, m + 1, r);
+    } else {
+        return recursive_binary_search(vect, x, l, m - 1);
+    };
+
+    return -1;
+};
 
 
 int main()
 {
     int x;
+ 
+    std::cout << "x: ";
     std::cin >> x;
 
+
     std::string input_line;
+
+    std::cout << "vector: ";
+    std::cin.ignore();
     std::getline(std::cin, input_line);
+
     std::vector<int> nums = fill_vector_from_string(input_line);
 
-    iterative_binary_search(nums, x);
+    std::cout << "Index of x is " << iterative_binary_search(nums, x) << "\n";
+    std::cout << "Index of x is " << recursive_binary_search(nums, x, 0 , static_cast<int>(nums.size())) << "\n";
+
     return 0;
 };
