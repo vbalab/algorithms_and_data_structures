9ca2723dd4789b9a81fcb6539017baf19fbeff46
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-08 22:57:43 +0700

    memoization solution

---
+++ algorithms/dynamic_prog.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#define MAX 1000
+
+
+// divide&conquer   for separable subproblems
+// dynamic          for overlapping subproblems
+
+int recursive_fib(const int n)
+{
+    if (n <= 1)
+        return 1;
+    return recursive_fib(n - 1) + recursive_fib(n - 2);
+};
+
+
+// memoization (top-down) - first time solving a problem then storing to use later
+int lookup[MAX];
+void _initialize()
+{
+    for (int i = 0; i < MAX; ++i)
+        lookup[i] = -1;
+};
+
+int memoization_fib(const int n)
+{
+    if (lookup[n] == -1) {
+        if (n <= 1)
+            lookup[n] = 1;
+        else
+            lookup[n] = memoization_fib(n - 1) + memoization_fib(n - 2);
+    }
+
+    return lookup[n];
+};
+
+
+// tabulation (bottom-top) - linearly calculate before solving a problem to look up after
+
+
+int main()
+{
+    _initialize();
+
+    int n;
+    std::cin >> n;
+
+    std::cout << recursive_fib(n) << "\n";
+    std::cout << memoization_fib(n) << "\n";
+
+};
