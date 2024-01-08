83d963b0446a0c5ff22c5002e555f38523474ab1
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-08 21:35:13 +0700

    will do later

--- problems/004_median_of_two_sorted_arrays.cpp
+++ problems/004_median_of_two_sorted_arrays.cpp
@@ -9,7 +9,7 @@ using namespace utils;
 class Solution {
 public:
     double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
-        // hard yet.
+        // hard yet
     };
 };
 
