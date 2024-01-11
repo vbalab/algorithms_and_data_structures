f0615a62fd41be24a3f8348abf892393428e8e86
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-11 12:11:23 +0700

    solve with 100% by runtime

--- problems/1768_merge_strings_alternately.cpp
+++ problems/1768_merge_strings_alternately.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <string>
-
+#include <cmath>
 
 class Solution
 {
     public:
     std::string mergeAlternately(std::string word1, std::string word2)
     {
+        std::string merged;
 
+        std::string& shortest = (word1.size() < word2.size()) ? word1 : word2;
+        std::string& longest = (word1.size() >= word2.size()) ? word1 : word2;
+
+        int i;
+
+        for (i = 0; i < shortest.size(); i++)
+        {
+                merged.push_back(word1[i]);
+                merged.push_back(word2[i]);
+        };
+
+        for (; i < longest.size(); i++)
+                merged.push_back(longest[i]);
+
+        return merged;  
     };
 };
 
