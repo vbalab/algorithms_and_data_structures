cbf5e491a43e91b21ff1961dc569fee4875c8d64
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-11 11:04:30 +0700

    set up the problem

---
+++ problems/1768_merge_strings_alternately.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+
+class Solution
+{
+    public:
+    std::string mergeAlternately(std::string word1, std::string word2)
+    {
+
+    };
+};
+
+
+int main()
+{
+    std::string word1;
+    std::cin >> word1;
+
+    std::string word2;
+    std::cin >> word2;
+
+
+    Solution solution;
+    std::cout << solution.mergeAlternately(word1, word2);
+
+    return 0;
+};
