1980346d89a4a2182fd7faf9877f291fa56f5c4d
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-12 18:56:26 +0300

    solve with 86% by runtime

--- problems/0605_can_place_flowers.cpp
+++ problems/0605_can_place_flowers.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include "utils.h"
+#include "../utils.h"
 
 
 using namespace utils;
@@ -11,7 +11,33 @@ class Solution
     public:
     bool canPlaceFlowers(std::vector<int>& flowerbed, int n)
     {
+        int length = flowerbed.size();
+        int left;
+        int right;
 
+
+        if (n > length / 2 + 1)
+            return false;
+
+        for (int i = 0; i < length; i++)
+        {
+            try {left = flowerbed.at(i - 1);}
+            catch (std::out_of_range) {left = 0;}
+
+            try {right = flowerbed.at(i + 1);}
+            catch (std::out_of_range) {right = 0;}
+
+            if (left == 0 and flowerbed[i] == 0 and right == 0)
+            {
+                flowerbed[i] = 1;
+                n -= 1;
+            }
+
+            if (n <= 0)
+                return true;
+        };
+
+        return false;
     };
 };
 
