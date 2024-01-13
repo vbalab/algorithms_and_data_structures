1e379823fff177156f58b7702d98df42c2ef8985
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-13 20:40:43 +0300

    solve

---
+++ problems/0345_reverse_vowels_of_a_string.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+
+
+class Solution
+{
+    public:
+    std::string reverseVowels(std::string s)
+    {
+        std::set<char> vowels { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' };
+        int j = s.size();   // s.size() - 1 already in the loop
+
+
+        for (int i = 0; i < j; i++)
+        {
+            if (vowels.find(s[i]) != vowels.end())
+            {
+                for (; j > i;)
+                {
+                    j--;
+
+                    if (vowels.find(s[j]) != vowels.end())
+                    {
+                        std::swap(s[i], s[j]);
+
+                        break;
+                    };
+                };
+            };
+        };
+
+        return s;
+    };
+};
+
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+
+    Solution solution;
+    std::cout << solution.reverseVowels(s) << "\n";
+
+    return 0;
+};
