a63f0fcee9304c2a612f25c398f0807c4fef514c
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-14 12:18:25 +0300

    create file for second solution

---
+++ problems/0151_reverse_words_in_a_string-first.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <sstream>
+
+
+class Solution
+{
+    public:
+    std::string reverseWords(std::string s)
+    {
+        removeExtraWhitespaces(s);
+
+        std::stringstream ss(s);
+
+        std::string reversed = "";
+        std::string word;
+
+        ss >> reversed;
+        while(ss >> word)
+        {
+            reversed = word + ' ' + reversed;
+        };
+        
+        return reversed;
+    };
+
+    void removeExtraWhitespaces(std::string & s)
+    {
+        int lenght = s.size();
+
+        for (int i = 0; i < lenght - 1;)
+        {
+
+            if (s[i] == ' ' and s[i + 1] == ' ')
+            {
+                s.erase(i, 1);
+                lenght--;
+            }
+            else
+                i++;
+        };
+
+        if (s[0] == ' ')
+            s.erase(0, 1);
+
+
+        if (s[lenght - 1] == ' ')
+            s.erase(lenght - 1, 1);
+    };
+};
+
+
+int main()
+{
+    std::string s;
+    // std::cin >> s;                                   // WRONG - doesn't catch after ' '
+    std::getline(std::cin, s);
+
+    Solution solution;
+    std::cout << solution.reverseWords(s) << '\n';
+
+    return 0;
+};
