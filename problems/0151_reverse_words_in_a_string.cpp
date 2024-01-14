a86932a434461282a00b8a97abd3a0ea6494f82f
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-01-14 10:57:29 +0300

    prepare for solving

---
+++ problems/0151_reverse_words_in_a_string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+class Solution
+{
+    public:
+    std::string reverseWords(std::string s)
+    {
+        for 
+        return s;
+    };
+};
+
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+
+    Solution solution;
+    std::cout << solution.reverseWords(s);
+
+    return 0;
+};
