5c3b6617fad79b6847a9128cac30baa83487c998
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-17 23:07:10 +0300

    prepare

---
+++ problems/2390_removing_stars_from_a_string.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+
+class Solution {
+public:
+    std::string removeStars(std::string s) {
+
+    };
+};
+
+
+int main() {
+    std::string s;
+    std::cin >> s;
+
+    Solution solution;
+    std::cout << solution.removeStars(s) << '\n';
+
+
+    return 0;
+};
