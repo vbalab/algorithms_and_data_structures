31aaa0a79510317f4a612ca394b2e6864c75c478
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-22 18:29:41 +0300

    WOW

--- data_structures/pointer.cpp
+++ data_structures/pointer.cpp
@@ -16,12 +16,15 @@ int main() {
     Some* ptr;
 
     // 2 main ways of dynamically (i.e. while program already running) allocate memory for pointer
-    ptr = new Some(2);                            // prt = new cast_type;
-    ptr = (Some*) malloc(sizeof(Some(2)));        // ptr = (cast_type*) malloc( num_bytes );
     // difference: new initializes after allocation, malloc() just allocates memory
 
+    ptr = new Some(2);                            // prt = new cast_type;
+    std::cout << ptr->data << " is the same as " << (*ptr).data << '\n';    // 2 is the same as 2
+    
+    
+    ptr = (Some*) malloc(sizeof(Some(2)));        // ptr = (cast_type*) malloc( num_bytes );
+    std::cout << ptr->data << " is the same as " << (*ptr).data << '\n';    // 0 is the same as 0 <- since no initilization
 
-    std::cout << ptr->data << " is the same as " << (*ptr).data << '\n';
 
     return 0;
 };
