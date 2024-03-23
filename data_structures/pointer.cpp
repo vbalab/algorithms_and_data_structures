834d994efae7b346e81c61799a8bcd6f96a8fead
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-23 22:51:22 +0300

    info about delete

--- data_structures/pointer.cpp
+++ data_structures/pointer.cpp
@@ -18,13 +18,16 @@ int main() {
     // 2 main ways of dynamically (i.e. while program already running) allocate memory for pointer
     // difference: new initializes after allocation, malloc() just allocates memory
 
-    ptr = new Some(2);                            // prt = new cast_type;
+    ptr = new Some(2);                              // prt = new cast_type;
     std::cout << ptr->data << " is the same as " << (*ptr).data << '\n';    // 2 is the same as 2
-    
-    
-    ptr = (Some*) malloc(sizeof(Some(2)));        // ptr = (cast_type*) malloc( num_bytes );
+
+    ptr = (Some*) malloc(sizeof(Some(2)));          // ptr = (cast_type*) malloc( num_bytes );
     std::cout << ptr->data << " is the same as " << (*ptr).data << '\n';    // 0 is the same as 0 <- since no initilization
 
+
+    // IF: ptr = nullptr    , then memory leak, do NOT do this 
+
+    delete ptr;                                     // we can 'delete' only dynamically allocated variables
 
     return 0;
 };
