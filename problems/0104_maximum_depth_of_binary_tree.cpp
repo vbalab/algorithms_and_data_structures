675aaea15852084cb76da21309c8f91e4837133f
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-25 18:12:01 +0300

    solve with 100% by runtime

---
+++ problems/0104_maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+class Node {
+public:
+    int val;
+
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        this->val = val;
+
+        this->left = nullptr;
+        this->right = nullptr;
+    };
+};
+
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr)
+            return 0;
+
+        return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+    };
+};
+
+
+int main() {
+    Node* root = new Node(0);
+    root->left = new Node(0);
+    root->right = new Node(0);
+    root->right->right = new Node(0);
+    root->right->right->left = new Node(0);
+
+
+    Solution solution;
+    std::cout << solution.maxDepth(root) << '\n';
+
+
+    return 0;
+};
