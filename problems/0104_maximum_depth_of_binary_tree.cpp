c51a6155a7f0a073482ecc14b0c56aee5b5e1422
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-25 18:12:25 +0300

    solve with 100% by runtime

--- problems/0104_maximum_depth_of_binary_tree.cpp
+++ problems/0104_maximum_depth_of_binary_tree.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 
+static const int _ = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr); 
+    std::cout.tie(nullptr);
+
+    return 0;
+}();
+
+
 class Node {
 public:
     int val;
