0e7987e737c38ed3c27bb504cf3ff44970dc33d9
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-04-13 21:58:29 +0300

    solve

--- problems/leetcode/0215_kth_largest_element_in_an_array.cpp
+++ problems/leetcode/0215_kth_largest_element_in_an_array.cpp
@@ -1,21 +1,44 @@
-#include <queue>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
 
 
 class Solution {
 public:
     int findKthLargest(std::vector<int>& nums, int k) {
-        std::queue<int> q;
+        std::make_heap(nums.begin(), nums.end());
 
-        for (int num: nums) {
-            if (q.back() < )
-        }
+        k--;
+        while (k--) {
+            std::pop_heap(nums.begin(), nums.end());
+            nums.pop_back();
+        };
+        
 
-        return q->back();
-    }
+        return nums.front();
+    };
 };
 
+
 int main() {
+    int input;
+    std::string input_line;
+    std::istringstream stream;
+    std::vector<int> v;
+
+
+    std::getline(std::cin, input_line);
+
+    stream.str(input_line);
+    while (stream >> input)
+        v.push_back(input);
+
+
+    Solution solution;
+    std::cout << solution.findKthLargest(v, 3);
+
+
     return 0;
 };
