82abc9a578ead1d7d6033762f8052af2b853fc2e
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-05-27 21:56:25 +0300

    create files

---
+++ data_structures/cpp_specific/vector.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+
+int main () {
+    std::vector<int> a[5];              // c array of 5 std::vectors
+    // std::vector<int> b(5);              // std::vector of size 5
+    // std::vector<int> c(5, -10);         // std::vector of size 5 filled with -10
+
+    return 0;
+}
---
+++ data_structures/graph.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+
+
+/*
+Representation: 
+    1) Adjacency Matrix, 
+    2) Adjacency List - [index i contains a linked list of the vertices adjacent to vertex i]
+
+Transope [ O(V+E) ] - revers of all edges in directed graph.
+*/
+
+
+class Graph {
+public:
+    std::vector<std::vector<int>> adj_list;
+
+    Graph() {};
+
+    add
+};
+
+
+int main() {
+    Graph graph;
+
+
+
+    return 0;
+};
